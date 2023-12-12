6ba2e6afaec1d83f3da5b81cc8e6621038c3414c
Joe di Stefano <joeydi@okaypl.us>
2023-12-12 10:05:32 -0500

    optimize images and add remaining sections

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
             <main>
                 <section className="hero">
                     <div className="container">
-                        <img src="/hero.png" width="3580" height="200" />
+                        <img src="/hero.png" width="3580" height="2000" />
                     </div>
                 </section>
                 <section className="usp-features">
@@ -19,6 +19,26 @@ function App() {
                     </div>
                 </section>
                 <Features />
+                <section className="benefits">
+                    <div className="container">
+                        <img src="/benefits.png" width="3580" height="2098" />
+                    </div>
+                </section>
+                <section className="specs">
+                    <div className="container">
+                        <img src="/specs.png" width="3580" height="2100" />
+                    </div>
+                </section>
+                <section className="conclusion">
+                    <div className="container">
+                        <img src="/conclusion.png" width="3580" height="520" />
+                    </div>
+                </section>
+                <section className="contact">
+                    <div className="container">
+                        <img src="/contact.png" width="3580" height="2252" />
+                    </div>
+                </section>
             </main>
             <footer>
                 <img src="/footer.png" width="3840" height="2441" />
